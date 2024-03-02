e6d6d3fe65d9568713328f2f46d1b18c0b0c4510
j0eun <kim950210@naver.com>
2024-03-02 10:56:28 +0900

    fixed typo

--- _2753/src/main.rs
+++ _2753/src/main.rs
@@ -6,13 +6,13 @@ fn main() {
 
     let year: u32 = buffer.trim().parse().unwrap();
 
-    let mut is_leaf_year = false;
+    let mut is_leap_year = false;
 
     if year % 400 == 0 || (year % 100 != 0 && year % 4 == 0) {
-        is_leaf_year = true;
+        is_leap_year = true;
     }
 
-    if is_leaf_year {
+    if is_leap_year {
         println!("1");
     } else {
         println!("0");
